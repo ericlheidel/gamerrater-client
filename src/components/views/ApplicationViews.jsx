23dad56afe3b0a7653053ab8deb92375f5e276bd
Eric Heidel <ericleeheidel@yahoo.com>
2024-03-25 16:31:08 -0500

    Begin User Token

--- src/components/views/ApplicationViews.jsx
+++ src/components/views/ApplicationViews.jsx
@@ -2,8 +2,17 @@ import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom"
 // import App from "../../App.jsx"
 import { NavBar } from "../nav/Navbar.jsx"
 import { AllGamesList } from "../games/AllGamesList.jsx"
+import { useEffect, useState } from "react"
 
 export const ApplicationViews = () => {
+  const [currentUserToken, setCurrentUserToken] = useState("")
+
+  useEffect(() => {
+    const localGamerUser = localStorage.getItem("gamer_token")
+    const gamerUser = JSON.parse(localGamerUser)
+    setCurrentUserToken(gamerUser.token)
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,7 +25,10 @@ export const ApplicationViews = () => {
             </>
           }
         >
-          <Route path="/allgames" element={<AllGamesList />} />
+          <Route
+            path="/allgames"
+            element={<AllGamesList currentUserToken={currentUserToken} />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/services/gamesService.jsx
+++ src/services/gamesService.jsx
@@ -1,3 +1,7 @@
 export const getAllGames = () => {
-  return fetch("http://localhost:8000/games").then((res) => res.json())
+  return fetch("http://localhost:8000/games", {
+    headers: {
+      Authorization: "Token 0be249c88238743e5b4a7ac370b5145730c28e20",
+    },
+  }).then((res) => res.json())
 }
